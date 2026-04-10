f6f418256448e1df9d32d18f895ba1d61080f948
Nagendra Katta <nagendrakatta2003@gmail.com>
2026-04-10 19:34:01 +0530

    Fixed Header in Products Page

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -6,13 +6,12 @@ import Header from '../Header'
 import './index.css'
 
 const Products = () => (
-  <>
+  <div className="product-page">
     <Header />
     <div className="product-sections">
       <PrimeDealsSection />
       <AllProductsSection />
     </div>
-  </>
+  </div>
 )
-
 export default Products
